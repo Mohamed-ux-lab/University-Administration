f96c05c3ea168aee96894e211313db8fea64e960
Mohamed-ux-lab <koureissym@gmail.com>
2024-10-16 23:05:00 +0000

    Separation des urls 2

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,8 @@ from . views import *
 app_name = 'accounts'
 
 urlpatterns = [
-    # path('login/', login_user, name='login'),
+    path('login/', login_user, name='login'),
     # path('register/', register_user, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
