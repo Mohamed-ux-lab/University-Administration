46dc935057ca65bf3c51161de240e75d8cb5500c
Mohamed-ux-lab <koureissym@gmail.com>
2024-10-17 11:06:08 +0000

    modification

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', login_user, name='login'),
     # path('register/', register_user, name='register'),
-    # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- studentAdministration/urls.py
+++ studentAdministration/urls.py
@@ -1,14 +1,16 @@
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path, include
 
-from .views import index, dashboard
+from .views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: redirect('accounts:login')),
+    # path('', lambda request: redirect('accounts:login')),
     path('', dashboard, name='dashboard'),
     path('etudiant/', include("etudiant.urls", namespace='etudiant')),
     path('professeur/', include("professeur.urls", namespace='professeur')),
     path('accounts/', include("accounts.urls", namespace='accounts')),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
